c66475d2dab5152b565afbe5663248e40ac9799e
fadyfouad <fady.fouad.a@gmail.com>
2019-05-23 19:35:59 +0200

    EqualsMethod

--- src/com/etaTech/SetAndHashSet/Challenge.java
+++ src/com/etaTech/SetAndHashSet/Challenge.java
@@ -6,27 +6,7 @@ import java.util.Objects;
  *** Created by Fady Fouad on 5/22/2019 at 21:30.***
  ***************************************************/
 public class Challenge {
-    private final String name ;
-
-    public Challenge(String name) {
-        this.name = name;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Challenge challenge = (Challenge) o;
-        String obName = challenge.getName();
-        return name.equals(obName);
-    }
-
-    @Override
-    public int hashCode() {
-        return name.hashCode();
+    public static void main(String[] args) {
+        System.out.println("================Challenge==============");
     }
 }
--- src/com/etaTech/SetAndHashSet/MainAnimal.java
+++ src/com/etaTech/SetAndHashSet/MainAnimal.java
@@ -1,5 +1,8 @@
 package com.etaTech.SetAndHashSet;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /****************************************************
  *** Created by Fady Fouad on 5/22/2019 at 21:42.***
  ***************************************************/
@@ -11,6 +14,27 @@ public class MainAnimal {
 
         System.out.println(dog1.equals(dog));
         System.out.println(dog.equals(dog1));
+
+        System.out.println("======================================================\n");
+        Set<Integer> squers = new HashSet<>();
+        Set<Integer> cubes = new HashSet<>();
+
+        for (int i = 0; i < 10; i++) {
+            squers.add(i * i);
+            cubes.add(i * i * i);
+        }
+        System.out.println(squers);
+        System.out.println(cubes);
+
+        Set<Integer>union = new HashSet<>(squers);
+        System.out.println(union);
+        union.addAll(cubes);
+        System.out.println(union.size());
+        System.out.println(union);
+        System.out.println("Intersections");
+        Set<Integer>intersections= new HashSet<>(squers);
+        intersections.retainAll(cubes);
+        System.out.println("\t"+intersections);
 
     }
 }
