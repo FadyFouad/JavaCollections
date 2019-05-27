82b7ea5e006e6b158a2471474de03bc609b02d37
fadyfouad <fady.fouad.a@gmail.com>
2019-05-27 15:07:34 +0200

    SstockList

--- src/com/etaTech/SortedCollections/StockList.java
+++ src/com/etaTech/SortedCollections/StockList.java
@@ -8,28 +8,30 @@ import java.util.Map;
  *** Created by Fady Fouad on 5/26/2019 at 17:41.***
  ***************************************************/
 public class StockList {
-    private final Map<String,StockItems>stockItems;
+    private final Map<String, StockItems> stockItems;
 
     public StockList() {
-        this.stockItems=new HashMap<>();
+        this.stockItems = new HashMap<>();
     }
-    public int addStock(StockItems item){
-        if (item!=null){
-            StockItems itemInStock = stockItems.getOrDefault(item.getName(),item);
-        if (itemInStock!=item){
-            item.setQuantityStock(itemInStock.getQuantityStock());
-        }
-        stockItems.put(item.getName(),item);
+
+    public int addStock(StockItems item) {
+        if (item != null) {
+            StockItems itemInStock = stockItems.getOrDefault(item.getName(), item);
+            if (itemInStock != item) {
+                item.setQuantityStock(itemInStock.getQuantityStock());
+            }
+            stockItems.put(item.getName(), item);
             return item.getQuantityStock();
         }
         return 0;
     }
-    public int sellStock(StockItems item,int quantity){
 
-        StockItems inStock= stockItems.getOrDefault(item,null);
-        if (inStock!=null && inStock.getQuantityStock()>=quantity&&(quantity>0)){
-         inStock.setQuantityStock(-quantity);
-         return quantity;
+    public int sellStock(StockItems item, int quantity) {
+
+        StockItems inStock = stockItems.getOrDefault(item, null);
+        if (inStock != null && inStock.getQuantityStock() >= quantity && (quantity > 0)) {
+            inStock.setQuantityStock(-quantity);
+            return quantity;
         }
         return 0;
     }
@@ -37,7 +39,8 @@ public class StockList {
     public Map<String, StockItems> getStockItems() {
         return Collections.unmodifiableMap(stockItems);
     }
-    public StockItems get(String key){
+
+    public StockItems get(String key) {
         return stockItems.get(key);
     }
 
@@ -47,15 +50,14 @@ public class StockList {
         double totalCoast = 0.0;
         for (Map.Entry<String, StockItems> itemsEntry :
                 stockItems.entrySet()) {
-            s+= "Name: " + itemsEntry.getValue().getName();
-            s+= "\tPrice: " + itemsEntry.getValue().getPrice()+"\n";
-            totalCoast+=itemsEntry.getValue().getPrice()*itemsEntry.getValue().getQuantityStock();
+            s += "Name: " + itemsEntry.getValue().getName();
+            s += "\tPrice: " + itemsEntry.getValue().getPrice() + "\n";
+            totalCoast += itemsEntry.getValue().getPrice() * itemsEntry.getValue().getQuantityStock();
         }
         return "StockList{" +
                 "stockItems=" + stockItems +
-                '}'+"\n"
-                +s+"\n"
-                +"Total Price : "+totalCoast;
-
+                '}' + "\n"
+                + s + "\n"
+                + "Total Price : " + totalCoast;
     }
 }
